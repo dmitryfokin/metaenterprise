9a4e81040e55d78f02940a698d09070e4bcad2b1
Dmitry Fokin <dkoder.dvl@gmail.com>
2022-03-26 19:08:34 +0300

    update

--- metaenterprise.js
+++ metaenterprise.js
@@ -1,13 +1,7 @@
 'use strict';
 
 const Enterpris = require('./lib/enterprise.js');
-// const MasterDataManager = require('./lib/appliedsObjects/masterDataManager.js');
-// const MasterData = require('./lib/appliedsObjects/masterData.js');
-// const Database = require('./lib/db/database.js');
 
 module.exports = {
   ...Enterpris, 
-  // ...MasterDataManager, 
-  // ...MasterData,
-  // ...Database, 
 }
